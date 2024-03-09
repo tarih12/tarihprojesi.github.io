6bb15655ba6e0a9ee1bf38b91c4da9063098f8d5
tarih12 <162534835+tarih12@users.noreply.github.com>
2024-03-09 19:14:23 +0300

    Add files via upload

---
+++ anasayfa.js
@@ -0,0 +1,4 @@
+document.getElementById("anasayfaLink").addEventListener("click", function(event) {
+    event.preventDefault(); // Sayfa yenilemesini engelle
+    window.scrollTo(0, 0); // Sayfanın en başına git
+});
